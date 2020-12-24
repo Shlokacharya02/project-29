b7da1d328adfddb7ecb54ad2cb8a57d5b513d1b4
Shlokacharya02 <75475646+Shlokacharya02@users.noreply.github.com>
2020-12-24 14:14:12 +0530

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,38 @@
+class Slingshot {
+    constructor(bodyA, pointB) {
+      var options = {
+        bodyA: bodyA,
+        pointB: pointB,
+        stiffness: 0.04,
+        length: 10,
+      };
+  
+      this.pointB = pointB;
+      this.sling = Constraint.create(options);
+      World.add(world, this.sling);
+    }
+
+    attach(){
+
+
+        
+    }
+
+    fly(){
+
+        this.sling.bodyA = null
+    }
+
+    display() {
+      if (this.sling.bodyA) {
+        var pointA = this.sling.bodyA.position;
+        var pointB = this.pointB;
+       stroke("white");
+       strokeWeight(3);
+        line(pointB.x, pointB.y, pointA.x, pointA.y);
+  
+       
+      }
+    }
+  }
+  
